589fbad77177355db5fab8dee0e8217cb5c329b1
congachu <songwlals050328@gmail.com>
2025-08-20 03:29:12 +0900

    fix: redisConfigannotation

--- src/main/java/com/emomap/emomap/common/jwt/RedisConfig.java
+++ src/main/java/com/emomap/emomap/common/jwt/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.emomap.emomap.common.jwt;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories(basePackages = "com.emomap.emomap")
+@ConditionalOnProperty(name = "spring.data.redis.host")
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
