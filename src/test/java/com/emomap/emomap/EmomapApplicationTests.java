8454fa3d8b3bd248cfd2852d9812b192e0f9f3b6
congachu <songwlals050328@gmail.com>
2025-08-20 03:27:37 +0900

    fix: test redis ignore

--- src/test/java/com/emomap/emomap/EmomapApplicationTests.java
+++ src/test/java/com/emomap/emomap/EmomapApplicationTests.java
@@ -1,9 +1,12 @@
 package com.emomap.emomap;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class})
 class EmomapApplicationTests {
 
     @Test
