e9e6ea98a43a7c994b25ad2c45b7e2273d1bc8fd
congachu <songwlals050328@gmail.com>
2025-08-20 01:54:17 +0900

    fix: RefreshTokenRepository

--- src/main/java/com/emomap/emomap/common/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/com/emomap/emomap/common/jwt/repository/RefreshTokenRepository.java
@@ -3,5 +3,5 @@ package com.emomap.emomap.common.jwt.repository;
 import com.emomap.emomap.common.jwt.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 }
